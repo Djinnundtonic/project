50cd003738bdfef278c116c2b9c531b26e955c91
Djinnundtonic <westcottferguson@ucdavis.edu>
2019-10-17 18:51:19 -0700

    Update rock:paper:scissors.py

--- rock:paper:scissors.py
+++ rock:paper:scissors.py
@@ -21,3 +21,20 @@ else:
 
 print("user choice is: " + choice_name)
 print("\nNOw its the computer turn....")
+while choice > 3 or choice < 1:
+    choice = int(input("enter number choice"))
+     # initialize value of choice_name variable 
+    # corresponding to the choice value 
+    if choice == 1:
+        choice_name = 'Rock'
+    elif choice === 2:
+        choice_name = 'Paper'
+    elif choice ==: 3
+        choice_name = 'Scissor'
+    print ("user choice is: + choice_name)
+    print (\n " now it's the Computers' bitch!")
+  #not sure why values that are equal are ==
+  # Computer chooses randomly any number  
+    # among 1 , 2 and 3. Using randint method 
+    # of random module 
+    comp_choice = random.randint (1,3)
